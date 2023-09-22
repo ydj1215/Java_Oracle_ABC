3b61bb15a2efc3f9c0e49b8a6ac44bb8774fffe2
ydj1215 <ydjdj2@naver.com>
2023-09-22 11:51:42 +0900

    230920

---
+++ src/com/abc/jdbc/dto/PostsDTO.java
@@ -0,0 +1,68 @@
+package com.abc.jdbc.dto;
+
+public class PostsDTO {
+    private String id;
+    private String title;
+    private String currentTime;
+    private String content;
+    private String membersID;
+    private String likesCounts;
+
+    public PostsDTO(){
+
+    }
+
+    public PostsDTO(String title, String content, String membersID) {
+        this.title = title;
+        this.content = content;
+        this.membersID = membersID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getMembersID() {
+        return membersID;
+    }
+
+    public String getLikesCounts() {
+        return likesCounts;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setMembersID(String membersID) {
+        this.membersID = membersID;
+    }
+
+    public void setLikesCounts(String likesCounts) {
+        this.likesCounts = likesCounts;
+    }
+}
